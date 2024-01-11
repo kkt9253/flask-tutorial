3a587384a586a675c545a772797a771066490dc0
kimgyeongtak <kkt9253@naver.com>
2024-01-11 20:52:23 +0900

    읽기

--- flask/server.py
+++ flask/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 topics = [
     {'id': 1, 'title': 'html', 'body': 'html is ...'},
     {'id': 2, 'title': 'css', 'body': 'css is ...'},
-    {'id': 3, 'title': 'javascruot', 'body': 'javascript is ...'}
+    {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
 ]
 
 
@@ -34,10 +34,27 @@ def create():
     return 'Create'
 
 
-#@app.route('/read/<id:int>/')
-@app.route('/read/<id>/')
+@app.route('/read/<int:id>/') # <int:id> => 정수로 받음
 def read(id):
-    print(id)
-    return 'Read '+id
+    liTags = ''
+    title = ''
+    body = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+        if id == topic['id']: # <int:id>를 안쓰면 이곳에서 str로 받아서 안됨
+            title = topic['title']
+            body = topic['body']
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {liTags}
+            </ol>
+            <h2>{title}</h2>
+            {body}
+        </body>
+    </html>
+    '''
 
 app.run(debug=True)
